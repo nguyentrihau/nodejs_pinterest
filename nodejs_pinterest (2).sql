7d05bc44e2a80049ce8f792f3a3080bcfdffeaf9
nguyentrihau <nguyenchonhau@gmail.com>
2023-02-25 07:58:48 +0700

    up

--- nodejs_pinterest (2).sql
+++ nodejs_pinterest (2).sql
@@ -25,14 +25,32 @@ CREATE TABLE `comments` (
 DROP TABLE IF EXISTS `images`;
 CREATE TABLE `images` (
   `img_id` int NOT NULL AUTO_INCREMENT,
-  `img_name` varchar(150) NOT NULL,
   `user_id` int NOT NULL,
-  `img_time` datetime NOT NULL,
+  `img_time` date NOT NULL,
+  `img_name` longtext NOT NULL,
+  `path` varchar(150) NOT NULL,
   PRIMARY KEY (`img_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `images_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+INSERT INTO `images` (`img_id`, `user_id`, `img_time`, `img_name`, `path`) VALUES
+(7,	14,	'2023-02-24',	'My first upload img',	'1677266622218_floral-designs-3840x2160-10372.jpg'),
+(8,	14,	'2023-02-24',	'My first upload img',	'1677266660050_floral-designs-3840x2160-10372.jpg');
+
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission` (
+  `permission_name` varchar(100) NOT NULL,
+  `permission_value` int NOT NULL,
+  PRIMARY KEY (`permission_value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `permission` (`permission_name`, `permission_value`) VALUES
+('Banned user',	0),
+('Members',	1),
+('Editors',	2),
+('Moderators',	3),
+('Administrators',	4);
 
 DROP TABLE IF EXISTS `save`;
 CREATE TABLE `save` (
@@ -53,10 +71,18 @@ CREATE TABLE `users` (
   `user_id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `password` varchar(500) NOT NULL,
-  PRIMARY KEY (`user_id`)
+  `user_name` varchar(50) NOT NULL,
+  `age` int NOT NULL,
+  `avatar` longtext,
+  `permission` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`user_id`),
+  KEY `permission` (`permission`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`permission`) REFERENCES `permission` (`permission_value`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `users` (`user_id`, `email`, `password`) VALUES
-(2,	'abc@gmail.com',	'$2b$10$4JhxEUhsILjVOV10z1DgluKPW/3mFw1P6DKxMT3h4DPYm6gtxta66');
+INSERT INTO `users` (`user_id`, `email`, `password`, `user_name`, `age`, `avatar`, `permission`) VALUES
+(14,	'abc@gmail.com',	'$2b$10$3RQSMdq.RHVWK9JVggtz/e.ToVIQK75HAhHcxZuKp3com7n7psK7.',	'Thinh',	26,	'D:\\NODEJS27bt\\NODEJS_PINTEREST/public/img/avatar/avatardefault.png',	4),
+(15,	'abcd@gmail.com',	'$2b$10$g1xDinVJAjmIaoXcAyBALuJoE4vWJQX21lgbyMQCdIYQB.HbT1M1a',	'Thinh',	26,	'D:\\NODEJS27bt\\NODEJS_PINTEREST/public/img/avatar/avatardefault.png',	3),
+(16,	'abce@gmail.com',	'$2b$10$el.91ZNXNR2N5vmIQCeyiuw9g789qVWlRqhvMgY0tAAfXOCzRDElK',	'Thinh',	26,	'D:\\NODEJS27bt\\NODEJS_PINTEREST/public/img/avatar/avatardefault.png',	0);
 
--- 2023-02-23 05:29:08
+-- 2023-02-25 00:58:14
